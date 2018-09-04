ed993eef47af0b44f9e99c7e3e417ec549aceb70
alexk <lexxx.arian@gmail.com>
2018-09-04 16:54:18 +0800

    'init_project'

---
+++ src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction(Request $request)
+    {
+        return $this->render('@App/default/index.html.twig', [
+            'title' => 'Это страница со списком заявок'
+        ]);
+    }
+
+    /**
+     * @Route("/new-task", name="newTask")
+     */
+    public function createTaskAction(Request $request)
+    {
+        return $this->render('@App/default/newTask.html.twig', [
+            'title' => 'Это страница с новой заявкой'
+        ]);
+    }
+}
